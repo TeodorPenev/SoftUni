aa79dcf4c95d638e6b41ce187f70b79a22334440
Teodor Penev <t.g.penev@gmail.com>
2014-11-13 01:03:25 +0200

    add content

---
+++ Level 1/JavaScript Basics/HomeWorks/3. JavaScript Loops, Arrays, Strings/3.JavaScript Loops, Arrays, Strings/14.Palindromes/scripts/palindromesExtract.js
@@ -0,0 +1,17 @@
+﻿function findPalindromes(str) {
+    var words =str.split(/[\s.]+/);
+    var word = '';
+    var palindromes = [];
+     words = words.filter(Boolean);
+    for (var i = 0; i < words.length; i++) {
+        word = words[i].toLowerCase();
+        if (word != word.split('').reverse().join('')) {
+            continue;
+        } else {
+            palindromes.push(word);
+        }
+    }
+    console.log(palindromes.join(', '));
+}
+
+findPalindromes('There is a man, his name was Bob.');
