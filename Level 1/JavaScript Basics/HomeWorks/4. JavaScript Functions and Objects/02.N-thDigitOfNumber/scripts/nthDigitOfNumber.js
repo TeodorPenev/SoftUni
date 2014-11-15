8c3fa9c5b3806786783c18018d0f1b7b14992e2a
Teodor Penev <t.g.penev@gmail.com>
2014-11-15 20:09:41 +0200

    add content

---
+++ Level 1/JavaScript Basics/HomeWorks/4. JavaScript Functions and Objects/02.N-thDigitOfNumber/scripts/nthDigitOfNumber.js
@@ -0,0 +1,16 @@
+﻿function findNthDigit(arr) {
+    var pos = arr[0];
+    var num =String(Math.abs(arr[1])).replace('.','');
+    var result = num[num.length - pos];
+    if (result==undefined) {
+        console.log('undefined ');
+    } else {
+        console.log(result);
+    }
+}
+
+findNthDigit([1, 6]);
+findNthDigit([2, -55]);
+findNthDigit([6, 923456]);
+findNthDigit([3, 1451.78]);
+findNthDigit([6, 888.88]);
