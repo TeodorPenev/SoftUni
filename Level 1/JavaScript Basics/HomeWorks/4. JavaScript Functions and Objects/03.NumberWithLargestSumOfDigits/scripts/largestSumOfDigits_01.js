6d3913407705c8ed062524191d4f632207147c8b
Teodor Penev <t.g.penev@gmail.com>
2014-11-15 23:50:19 +0200

    add content

---
+++ Level 1/JavaScript Basics/HomeWorks/4. JavaScript Functions and Objects/03.NumberWithLargestSumOfDigits/scripts/largestSumOfDigits_01.js
@@ -0,0 +1,34 @@
+﻿function findLargestBySumOfDigits(arr) {
+    var max = 0;
+    var pos = 0;
+    var arrTemp = [];
+    for (var i = 0; i < arr.length; i++) {
+       arrTemp[i] = Math.abs(arr[i]);
+        if (arrTemp[i]>9 && arrTemp[i]<100 ) {
+            var tempF = String(arrTemp[i]);
+            var tempSum = parseInt(tempF[0]) + parseInt(tempF[1]);
+            if (tempSum>max) {
+                max = tempSum;
+                pos = i;
+            }
+        }
+        if (arr[i]> 99) {
+            var tempS = String(arrTemp[i]);
+            tempSum = parseInt(tempS[0]) + parseInt(tempS[1]) + parseInt(tempS[2]);
+            if (tempSum > max) {
+                max = tempSum;
+                pos = i;
+            }
+        }
+    }
+    if (max > 1) {
+        console.log(arr[pos]);
+    } else {
+        console.log(undefined);
+    }
+}
+
+findLargestBySumOfDigits([5, 10, 15, 111]);
+findLargestBySumOfDigits([33, 44, -99, 0, 20]);
+findLargestBySumOfDigits(['hello']);
+findLargestBySumOfDigits([5, 3.3]);
---
+++ Level 1/JavaScript Basics/HomeWorks/4. JavaScript Functions and Objects/03.NumberWithLargestSumOfDigits/scripts/largestSumOfDigits_02.js
@@ -0,0 +1,30 @@
+﻿function findLargestBySumOfDigits(arr) {
+    var max = 0;
+    var pos = 0;
+    var arrTemp = [];
+    var tempSum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        arrTemp[i] = Math.abs(arr[i]);
+        if (arrTemp[i]>9) {
+            var tempF = String(arrTemp[i]);
+            for (var j = 0; j < tempF.length; j++) {
+                tempSum = parseInt(tempSum) + parseInt(tempF[j]);
+                if (tempSum > max) {
+                    max = tempSum;
+                    pos = i;
+                }
+            }
+            tempSum = 0;
+        }
+    }
+    if (max > 1) {
+        console.log(arr[pos]);
+    } else {
+        console.log(undefined);
+    }
+}
+
+findLargestBySumOfDigits([5, 10, 15, 111]);
+findLargestBySumOfDigits([33, 44, -99, 0, 20]);
+findLargestBySumOfDigits(['hello']);
+findLargestBySumOfDigits([5, 3.3]);
