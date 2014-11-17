2de81badd8319c0c1bb56a63955fd369b405c139
Teodor Penev <t.g.penev@gmail.com>
2014-11-17 18:58:39 +0200

    add content

---
+++ Level 1/JavaScript Basics/Trash/TypingGame/TypingGame/5. DOM-and-Events-Demos/TraversingTheDOM.js
@@ -0,0 +1,4 @@
+﻿var studentsList = document.createElement("ul");
+studentsList.innerHTML = "Student: Pesho";
+var liElement = document.createElement("li"); studentsList.appendChild(studentLi);
+document.body.appendChild(studentsList);
---
+++ Level 1/JavaScript Basics/Trash/WebApplication1/WebApplication1/JavaScript1.js
@@ -0,0 +1,7 @@
+﻿(function() {
+    var studentsList = document.createElement("ul");
+    studentsList.innerHTML = "Student: Pesho";
+    var liElement = document.createElement("li"); studentsList.appendChild(studentLi);
+    document.body.appendChild(studentsList);
+})()
+
