84f27abb0b2aad82c6a51731f214b247304233b0
Teodor Penev <t.g.penev@gmail.com>
2015-01-24 15:47:04 +0200

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/1.Defining-Classes/01.Persons/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+    class Person
+    {
+        private string name;
+        private byte age;
+        private string email;
+
+        public Person(string name, byte age, string email)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Email = email;
+        }
+       
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name cannot be empty!");
+                }
+                this.name = value;
+                }
+        }
+
+        public byte Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            set {
+                if (value>100 || value<1)
+                {
+                    throw new ArgumentOutOfRangeException("Age got to be in range [1...100]!");
+                }
+                this.age = value;
+                 } 
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set {
+                if (value!="")
+                {
+                    if (value != null && !value.Contains("@"))
+                    {
+                        throw new ArgumentException("Invalid email!");
+                    }
+                }
+               
+                this.email = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Age: {1}, Email: {2}", this.name, this.age, this.email);
+        }
+    }
+
---
+++ Level 2/Object-Oriented Programming/HomeWorks/1.Defining-Classes/01.Persons/PersonsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Persons
+{
+    class PersonsTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter age: ");
+            byte age = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter email: ");
+            string email = Console.ReadLine();
+            Person testPerson = new Person(name,age,email);
+            Console.WriteLine(testPerson);
+        }
+    }
+}
