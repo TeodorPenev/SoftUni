279e52a4c4f9731e8bbb64dd4522e32289cee7c8
Teodor Penev <t.g.penev@gmail.com>
2015-02-11 00:38:48 +0200

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/2.Exception Handling/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Remoting.Services;
+
+namespace _01.SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter Integer Number: ");
+                int numI = int.Parse(Console.ReadLine());
+                string numStr = Convert.ToString(numI);
+                double numD = double.Parse(numStr);
+                double sqrareNum =Math.Sqrt(numD);
+                
+                if (numI < 0)
+                {
+                    throw new FormatException();
+                }
+                Console.WriteLine("Square root: {0}",sqrareNum);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
+
+
+        }
+    }
+}
