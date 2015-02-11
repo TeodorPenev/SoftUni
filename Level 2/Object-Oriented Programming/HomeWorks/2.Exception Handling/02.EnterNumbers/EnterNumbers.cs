455d3d82bd4ff9ebcae9d713d1a4e08525203f09
Teodor Penev <t.g.penev@gmail.com>
2015-02-11 03:09:26 +0200

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/2.Exception Handling/02.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _02.EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+           EnterNumbers.ReadNumber(2,25);
+        }
+        
+        public static void ReadNumber(int start, int end)
+        {
+            Console.Write("Please enter a number in range [{0}...{1}]: ",start,end);
+            int numInRange =int.Parse(Console.ReadLine());
+            if (numInRange<start || numInRange>end)
+            {
+                throw new FormatException("Invalid number.");
+            }
+           
+            int tempRem = 0;
+            Console.WriteLine("Enter 10 numbers a1, a2, … a10, such that 1 < a1 < … < a10 < 100: ");
+            for (int i = 0; i < 10; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                if (temp > 0 && tempRem < temp)
+                {
+                    tempRem = temp;
+                }
+                else
+                {
+                    Console.WriteLine("You enter invalid number please try again.");
+                    i--;
+                }
+            }
+            }
+            
+        }
+}
+
