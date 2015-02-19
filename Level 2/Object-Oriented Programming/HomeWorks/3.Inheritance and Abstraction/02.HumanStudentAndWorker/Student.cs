29f5caf94691c8c1cb47f14258a08a322a146a4c
Teodor Penev <t.g.penev@gmail.com>
2015-02-19 22:29:59 +0200

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/3.Inheritance and Abstraction/02.HumanStudentAndWorker/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.HumanStudentAndWorker
+{
+    class Student:Human
+    {
+        private string faultyNumber;
+
+        public Student(string firstName, string lastName, string faultyNumber) : base(firstName, lastName)
+        {
+            this.FaultyNumber = faultyNumber;
+        }
+
+        public string FaultyNumber
+        {
+            get { return this.faultyNumber; }
+            set
+            {
+                if (value.Length<4 || value.Length>9)
+                {
+                    throw new ArgumentOutOfRangeException("The faulty number got to be in range 5-10 digits / letters");
+                }
+                this.faultyNumber = value;
+            }
+        }
+    }
+}
---
+++ Level 2/Object-Oriented Programming/HomeWorks/3.Inheritance and Abstraction/02.HumanStudentAndWorker/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.HumanStudentAndWorker
+{
+    class Worker:Human
+    {
+        private double weekSalary;
+        private int workHoursPerDay;
+
+        public Worker(string firstName, string lastName, double weekSalary, int workHoursPerDay)
+            : base(firstName, lastName)
+        {
+            this.WeekSalary = weekSalary;
+            this.WorkHoursPerDay = workHoursPerDay;
+        }
+
+        public double WeekSalary
+        {
+            get { return this.weekSalary; }
+            set { this.weekSalary = value; }
+        }
+
+        public int WorkHoursPerDay
+        {
+            get { return this.workHoursPerDay; }
+            set { this.workHoursPerDay = value; }
+        }
+
+        public double MoneyPerHour()
+        {
+        double result = this.weekSalary/(this.WorkHoursPerDay*5);
+        return result;
+        }
+    }
+}
