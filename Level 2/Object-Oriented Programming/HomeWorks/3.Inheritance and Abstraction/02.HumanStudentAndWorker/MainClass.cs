508833f9485509c4349ddf9ce21896b37af6e4fd
Teodor Penev <t.g.penev@gmail.com>
2015-02-20 02:39:09 +0200

    add content

--- Level 2/Object-Oriented Programming/HomeWorks/3.Inheritance and Abstraction/02.HumanStudentAndWorker/MainClass.cs
+++ Level 2/Object-Oriented Programming/HomeWorks/3.Inheritance and Abstraction/02.HumanStudentAndWorker/MainClass.cs
@@ -10,6 +10,31 @@ namespace _02.HumanStudentAndWorker
     {
         static void Main(string[] args)
         {
+            List<Student> students = new List<Student>();
+
+            Student TeodorP = new Student("Teodor","Penev","115084");
+            Student Mihaela = new Student("Mihaela", "Markova", "115070");
+            Student Deriq = new Student("Deriq", "Halim", "115096");
+            Student Serap = new Student("Serap", "Nedjibova", "115124");
+            Student Desislava = new Student("Desislava", "Markovska", "115124");
+            Student Cvetelina = new Student("Cvetelina", "Asenova", "115024");
+            Student TeodorG = new Student("Teodor", "Georgiev", "112140");
+            Student Anatoli = new Student("Anatoli", "Petrov", "131440");
+            Student Liliq = new Student("Liliq", "Petkova", "113110");
+            Student Aleksandra = new Student("Aleksandra", "Karamfilova", "153110");
+
+            students.Add(TeodorP);
+            students.Add(Mihaela);
+            students.Add(Deriq);
+            students.Add(Serap);
+            students.Add(Desislava);
+            students.Add(Cvetelina);
+            students.Add(TeodorG);
+            students.Add(Anatoli);
+            students.Add(Liliq);
+            students.Add(Aleksandra);
+
+
         }
     }
 }
