150affff41b0c4f5d3eff9adf5372d0d1d7b8b6d
Teodor Penev <t.g.penev@gmail.com>
2015-03-12 05:10:09 +0200

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/5.Other Types in OOP(Enum,Struct,Generic,Attrib)/02.FractionCalculator/Fraction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.FractionCalculator
+{
+    struct Fraction
+    {
+        private long numerator;
+        private long denominator;
+        private long result;
+
+
+        public Fraction(long numerator,long denominator):this()
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+
+        public long Numerator
+        {
+            get { return this.numerator; }
+            set { this.numerator = value; }
+        }
+
+        public long Denominator
+        {
+            get { return this.denominator; }
+            set {
+                if (value==0)
+                {
+                    throw new ArgumentException("The dominator cannot be 0!");
+                }
+                this.denominator = value; }
+        }
+
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+            long numerator = fraction1.Numerator * fraction2.Denominator + fraction2.Numerator * fraction1.Denominator;
+            long denominator = fraction1.Denominator * fraction2.Denominator;
+
+            return new Fraction(numerator, denominator);
+        }
+
+        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+            long numerator = fraction1.Numerator * fraction2.Denominator - fraction2.Numerator * fraction1.Denominator;
+            long denominator = fraction1.Denominator * fraction2.Denominator;
+
+            return new Fraction(numerator, denominator);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(String.Format("{0}\n",(decimal)this.numerator / (decimal)this.denominator));
+            return result.ToString();
+        }
+    }
+}
