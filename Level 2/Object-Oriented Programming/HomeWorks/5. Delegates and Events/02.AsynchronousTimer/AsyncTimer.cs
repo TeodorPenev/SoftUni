8f37e8e08830038c97f9fdd6a2ea652bd9b9d186
Teodor Penev <t.g.penev@gmail.com>
2015-03-14 04:13:48 +0200

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/5. Delegates and Events/02.AsynchronousTimer/AsyncTimer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace _02.AsynchronousTimer
+{
+    internal class AsyncTimer
+    {
+        private int ticks;
+        private int tickNumber = 0;
+        private System.Timers.Timer timer;
+        private System.Action method;
+
+        public AsyncTimer(Action method, int ticks, int t)
+        {
+            this.Ticks = ticks;
+            this.timer = new System.Timers.Timer(t);
+            this.method = method;
+
+            this.timer.Elapsed += new ElapsedEventHandler(Action);
+
+            this.timer.Enabled = true;
+        }
+
+        private void Action(object source, ElapsedEventArgs e)
+        {
+            this.tickNumber++;
+            method();
+
+            if (this.tickNumber == this.ticks)
+            {
+                this.timer.Enabled = false;
+            }
+        }
+
+        public int Ticks
+        {
+            get { return this.ticks; }
+            set { this.ticks = value; }
+        }
+    }
+}
---
+++ Level 2/Object-Oriented Programming/HomeWorks/5. Delegates and Events/02.AsynchronousTimer/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02.AsynchronousTimer
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Action method = () => Console.WriteLine("--->");
+            AsyncTimer timer = new AsyncTimer(method, 15 , 1500);
+
+            int ticks = timer.Ticks;
+            Console.WriteLine("Try to throw arrow to right corner(use space)...you have {0} arrow!", ticks);
+            
+            Console.ReadLine();
+        }            
+    }
+}
