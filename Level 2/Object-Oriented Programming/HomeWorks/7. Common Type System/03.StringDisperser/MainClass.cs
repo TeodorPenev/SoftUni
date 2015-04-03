06600de3e428d920170f28204741455f30c19e81
Teodor Penev <t.g.penev@gmail.com>
2015-04-03 22:07:36 +0300

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/7. Common Type System/03.StringDisperser/MainClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.StringDisperser
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            StringDisperser args1 = new StringDisperser("gosho", "pesho", "tanio");
+            StringDisperser args2 = new StringDisperser("gosho", "pesho", "tanio");
+
+            Console.WriteLine("equals override");
+            bool test1 = Equals(args1, args2);       //equals override
+            Console.WriteLine(test1);
+
+
+            Console.WriteLine("operators override");
+            bool test2 = args1 != args2;             //operators override
+            Console.WriteLine(test2);
+
+            Console.WriteLine("IClonable");
+            StringDisperser test3 = (StringDisperser)args1.Clone();        //ICloneable
+            Console.WriteLine(test3);
+
+            Console.WriteLine("IComparable");
+            Array test4 = new []      //IComparable
+            {
+                new StringDisperser("gosho", "pesho", "gosho", "pesho", "tanio"),
+                new StringDisperser("gosho", "pesho", "tanio", "gosho", "pesho", "tanio","gosho", "pesho", "tanio","gosho", "pesho", "tanio"),
+                new StringDisperser("gosho", "pesho", "tanio","gosho", "pesho", "tanio","gosho", "pesho", "tanio"),
+                new StringDisperser("gosho", "pesho", "tanio")
+            };
+
+            Array.Sort(test4);
+            foreach (var arg in test4)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Console.WriteLine("IEnumerable");
+            foreach (var ch in args1)               //IEnumerable
+            {
+                Console.Write(ch + " ");
+            }
+
+        }
+    }
+}
---
+++ Level 2/Object-Oriented Programming/HomeWorks/7. Common Type System/03.StringDisperser/StringDisperser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace _03.StringDisperser
+{
+    class StringDisperser:ICloneable,IComparable<StringDisperser>,IEnumerable<StringDisperser>
+    {
+        List<char> letters = new List<char>();
+ 
+        public StringDisperser(params string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+                foreach (var letter in arg)
+                {
+                 letters.Add(letter);   
+                }
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            StringDisperser otherArg = obj as StringDisperser;
+           
+                if (this.letters.ToString().Equals(otherArg.letters.ToString()))
+                {
+                    return true;
+                }
+            
+            return false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(String.Format("{0}", String.Join("",this.letters.ToArray())));
+            return result.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return letters.GetHashCode();
+        }
+
+        public static bool operator ==(StringDisperser element1, StringDisperser element2)
+        {
+            return element1.Equals(element2);
+        }
+
+        public static bool operator !=(StringDisperser element1, StringDisperser element2)
+        {
+            return !(element1.Equals(element2));
+        }
+
+        public object Clone()
+        {
+            string obj = this.ToString();
+            return new StringDisperser(obj);
+        }
+
+        public int CompareTo(StringDisperser other)
+        {
+            return this.ToString().CompareTo(other.ToString());
+        }
+
+        public IEnumerator<StringDisperser> GetEnumerator()
+        {
+            for (int i = 0; i < this.letters.Count; i++)
+            {
+                yield return new StringDisperser(this.letters[i].ToString());
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
