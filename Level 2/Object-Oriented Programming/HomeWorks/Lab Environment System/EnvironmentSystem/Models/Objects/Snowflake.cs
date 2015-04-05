19ed89d05117b0b8157da6e6733d5b7b2e42ab27
Teodor Penev <t.g.penev@gmail.com>
2015-04-05 03:10:19 +0300

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/Lab Environment System/EnvironmentSystem/Models/Objects/Snowflake.cs
@@ -0,0 +1,18 @@
+﻿namespace EnvironmentSystem.Models.Objects
+{
+    public class Snowflake : MovingObject
+    {
+        protected const char SnowflakeCharImage = '*';
+
+        public Snowflake(int x, int y, int width, int height, Point direction)
+            : base(x, y, width, height, direction)
+        {
+            this.ImageProfile = this.GenerateImageProfile();
+        }
+
+        protected virtual char[,] GenerateImageProfile()
+        {
+            return new char[,] { { SnowflakeCharImage } };
+        }
+    }
+}
