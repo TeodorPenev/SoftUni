85d5928175af05252354f7cf5d2f5305fabc9b44
Teodor Penev <t.g.penev@gmail.com>
2015-04-30 18:02:07 +0300

    add content

---
+++ Level 2/Object-Oriented Programming/HomeWorks/Workshop Document System/Document_System/Document_System/CompositeElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Document_System
+{
+    public class CompositeElement : Element
+    {
+        public IList<Element> ChildElement { get; set; }
+
+        public CompositeElement()
+        {
+            this.ChildElement = new List<Element>();
+        }
+
+        public CompositeElement(params Element[] elements)
+            :this()
+        {
+            this.Add(elements);
+        }
+
+        public void Add(params Element[] elements)
+        {
+            foreach (var element in elements)
+            {
+                this.ChildElement.Add(element);
+            }
+        }
+        
+    }
+}
---
+++ Level 2/Object-Oriented Programming/HomeWorks/Workshop Document System/Document_System/Document_System/Element.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Document_System
+{
+    public abstract class Element
+    {
+        public IList<Element> ChildElement { get; set; }
+    }
+}
