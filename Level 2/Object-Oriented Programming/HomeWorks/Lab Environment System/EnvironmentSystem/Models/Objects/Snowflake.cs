7549c814c2d818dc65ecb10d80f341867bea5795
Teodor Penev <t.g.penev@gmail.com>
2015-05-18 18:20:23 +0300

    add content

--- Level 2/Object-Oriented Programming/HomeWorks/Lab Environment System/EnvironmentSystem/Models/Objects/Snowflake.cs
+++ Level 2/Object-Oriented Programming/HomeWorks/Lab Environment System/EnvironmentSystem/Models/Objects/Snowflake.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+
 namespace EnvironmentSystem.Models.Objects
 {
     public class Snowflake : MovingObject
@@ -14,27 +15,29 @@ namespace EnvironmentSystem.Models.Objects
 
         protected virtual char[,] GenerateImageProfile()
         {
-            return new char[,] { { SnowflakeCharImage } };
+            return new char[,] {{SnowflakeCharImage}};
         }
 
         public override void RespondToCollision(CollisionInfo collisionInfo)
         {
-            var hitObjectGroup = collisionInfo.HitObject.CollisionGroup;
-            if (hitObjectGroup == CollisionGroup.Ground || hitObjectGroup == CollisionGroup.Snow)
+            var hitObjectGroup = collisionInfo.HitObject;
+            if (hitObjectGroup is Ground)
             {
                 this.Exists = false;
             }
         }
-
-        public override IEnumerable<EnvironmentObject> ProduceObjects()
-        {
-            List<EnvironmentObject> producedObjects = new List<EnvironmentObject>();
-            if (!this.Exists)
-            {
-                producedObjects.Add(new Snow(this.Bounds.TopLeft.X, this.Bounds.TopLeft.Y - 1, 1, 1));
-            }
-
-            return producedObjects;
-        }
     }
 }
+
+//public override IEnumerable<EnvironmentObject> ProduceObjects()
+        //{
+        //    List<EnvironmentObject> producedObjects = new List<EnvironmentObject>();
+        //    if (!this.Exists)
+        //    {
+        //        producedObjects.Add(new Snow(this.Bounds.TopLeft.X, this.Bounds.TopLeft.Y - 1, 1, 1));
+        //    }
+
+        //    return producedObjects;
+        //}
+//    }
+//}
