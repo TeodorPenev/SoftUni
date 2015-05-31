7463507796e4bddd71fad9ba31e6b3c1bf1ffd56
Teodor Georgiev Penev <t.g.penev@gmail.com>
2015-05-31 20:56:56 +0300

    add content

---
+++ Level 2/Object-Oriented Programming/Preparation for Exam/Problem-0-Console-Forum/My Solution/Commands/Factories/LoginCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleForum.Contracts;
+using ConsoleForum.Entities.Users;
+using ConsoleForum.Utility;
+
+namespace ConsoleForum.Commands.Factories
+{
+    public class LoginCommand:AbstractCommand
+    {
+        public LoginCommand(IForum forum)
+            : base(forum)
+        {
+        }
+
+        public override void Execute()
+        {
+            var users = this.Forum.Users;
+            string username = this.Data[1];
+            string password = PasswordUtility.Hash(this.Data[2]);
+
+            if (Forum.IsLogged)
+            {
+                throw new CommandException(Messages.AlreadyLoggedIn);
+            }
+
+            var existingUser = users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (existingUser==null)
+            {
+                throw new CommandException(Messages.InvalidLoginDetails);
+            }
+
+            this.Forum.CurrentUser = existingUser;
+            this.Forum.Output.AppendFormat(Messages.LoginSuccess, existingUser.Username);
+        }
+    }
+}
---
+++ Level 2/Object-Oriented Programming/Preparation for Exam/Problem-0-Console-Forum/My Solution/Entities/Users/Adminstrator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleForum.Contracts;
+
+namespace ConsoleForum.Entities.Users
+{
+   public class Adminstrator:User, IAdministrator
+    {
+       public Adminstrator(int id, string name, string password, string email) : base(id, name, password, email)
+       {
+       }
+
+    }
+}
